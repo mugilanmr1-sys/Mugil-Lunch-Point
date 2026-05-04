24f0d526ccf52c37275e2ae98867786685b7c431
mugilanmr1-sys <mugilanmr1@gmail.com>
2026-05-04 08:31:09 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,7 @@ const ADMIN_SETTINGS = {
   forceShopOpenToday: true,
 
   // Add the IDs of the items you want to show for Afternoon
-  afternoonMenuIds: ["m1","m2","m3","m5","s1","s2,","s3"],
+  afternoonMenuIds: ["m1","m2","m3","m5","s1","s2","s3"],
 
   // Add the IDs of the items you want to show for Evening/Night
   nightMenuIds: ["t1", "t2", "t4", "s2"],
